9d56d2ead94a99e0727dbd83f0d1fa7f0eda1a39
gustavoscruz <gustadscruz@gmail.com>
2023-10-03 15:00:44 -0300

    modified Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,2 +1,35 @@
 public class Livro {
+    private String title;
+    private String author;
+    private int numberPages;
+
+    public Livro(String titulo, String autor, int numero){
+        this.title = titulo;
+        this.author = autor;
+        this.numberPages = numero;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String titulo){
+        this.title = titulo;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+
+    public void setAuthor(String autor){
+        this.author = autor;
+    }
+
+    public int getNumberPages(){
+        return this.numberPages;
+    }
+
+    public void setNumberPages(int numero){
+        this.numberPages = numero;
+    }
 }
