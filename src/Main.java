065d80e05af2aaa3a35e7b279e14ca27affd7584
gustavoscruz <gustadscruz@gmail.com>
2023-10-03 15:53:31 -0300

    Creation and uploaded Produto class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Carro gol = new Carro("Volkswagen", "Gol", 2020);
+        Carro jeta = new Carro("Volkswagen", "Jeta", 2015);
+        Carro jeep = new Carro("Jeep", "Renegade", 2021);
+        Carro palio = new Carro("Fiat", "Palio", 2012);
+        Carro creta = new Carro("Hyundai", "Creta", 2022);
 
+        System.out.println(gol);
+        System.out.println(jeta);
+        System.out.println(jeep);
+        System.out.println(palio);
+        System.out.println(creta);
     }
 }
