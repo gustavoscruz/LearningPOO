29de668c0aae3b47c9eabf5e946b039c8abbe57a
gustavoscruz <gustadscruz@gmail.com>
2023-10-03 14:43:57 -0300

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("Gustavo", 20);
-        Pessoa p2 = new Pessoa("Vinícius", 21);
-        Pessoa p3 = new Pessoa("Luan", 20);
-        Pessoa p4 = new Pessoa("Flakas", 15);
-        Pessoa p5 = new Pessoa("Alice", 10);
+
 
     }
 }
