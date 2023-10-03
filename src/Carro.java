12885fffef3eb947a51d2e2a13a56bcd88759cb9
gustavoscruz <gustadscruz@gmail.com>
2023-10-03 15:30:38 -0300

    modified class Carro

--- src/Carro.java
+++ src/Carro.java
@@ -1,2 +1,21 @@
 public class Carro {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Carro(String marca, String modelo, int fabricacao){
+        this.brand = marca;
+        this.model = modelo;
+        this.year = fabricacao;
+    }
+
+    public Carro(){
+        this.brand = "";
+        this.model = "";
+        this.year = 0;
+    }
+
+    public String toString(){
+        return "marca: " + this.brand + " modelo: " + this.model + " ano de fabricação: " + this.year;
+    }
 }
