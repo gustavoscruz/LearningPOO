f1554be4c9a0f5050d94d2fb91e975d5900a30c2
gustavoscruz <gustadscruz@gmail.com>
2023-10-03 15:15:09 -0300

    remove content of the main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Livro book1 = new Livro("rosa", "talento", 356);
-        System.out.println(book1.getTitle());
-        System.out.println(book1.getAuthor());
-        System.out.println(book1.getNumberPages());
 
-        book1.setTitle("Azul");
-        book1.setAuthor("Rosa");
-        book1.setNumberPages(555);
-
-        System.out.println(book1.getTitle());
-        System.out.println(book1.getAuthor());
-        System.out.println(book1.getNumberPages());
     }
 }
