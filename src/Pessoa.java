9eb71b3eb003cecedd864d48f87ea6a65eb49a98
gustavoscruz <gustadscruz@gmail.com>
2023-10-03 14:18:24 -0300

    Creation of getters e setters

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -7,4 +7,20 @@ public class Pessoa {
         this.idade = age;
     }
 
+    public getNome(){
+        return this.nome;
+    }
+
+    public setNome(String name){
+        this.nome = name;
+    }
+
+    public getIdade(){
+        return this.idade;
+    }
+
+    public setIdade(int age){
+        this.idade = age;
+    }
+
 }
