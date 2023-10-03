5448b4a2950c5c6cfb7d0b539882a5ceedef3a9a
gustavoscruz <gustadscruz@gmail.com>
2023-10-03 17:04:59 -0300

    Modified Triangulo class

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,2 +1,26 @@
 public class Triangulo {
+    private double base;
+    private double height;
+
+    public Triangulo(double b, double h){
+        this.base = b;
+        this.height = h;
+    }
+    public Triangulo(){
+    }
+
+    public double getBase(){
+        return this.base;
+    }
+
+    public double getBase(){
+        return this.height;
+    }
+
+    public double calculateArea(){
+        return this.base * this.height * 0.5;
+    }
+    public String toString(){
+        return "Base: " + this.base + "\nAltura: " + this.height + "\nÁrea: " + this.base * this.height * 0.5 + "\n";
+    }
 }
