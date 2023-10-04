a202997150b3c9b9622f1f106e12069cd958bd81
gustavoscruz <gustadscruz@gmail.com>
2023-10-04 18:07:01 -0300

    modified ATM class and ContaBancaria class

--- src/ATM.java
+++ src/ATM.java
@@ -1,2 +1,10 @@
 public class ATM {
+    public void deposit(ContaBancaria x, double valor){
+        x.setBalance(x.getBalance() + valor);
+    }
+    public void withdraw(ContaBancaria x, double valor){
+        x.setBalance(x.getBalance() - valor);
+    }
+
+
 }
--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,2 +1,19 @@
 public class ContaBancaria {
+    private String accountNumber;
+    private double balance;
+
+    public ContaBancaria(String number, double saldo){
+        this.accountNumber = number;
+        this.balance = saldo;
+    }
+
+    public String getAccountNumber(){
+        return this.accountNumber;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+    public void setBalance(double valor){
+        this.balance = valor;
+    }
 }
