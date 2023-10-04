63d3770cdbb845de43a3418d3c26a9afd1c4f730
gustavoscruz <gustadscruz@gmail.com>
2023-10-04 15:40:46 -0300

    modified Aluno class

--- src/Aluno.java
+++ src/Aluno.java
@@ -3,6 +3,14 @@ public class Aluno {
     private String registration;
     private double grade1, grade2, grade3;
 
+    public Aluno(String nome, String matricula, double n1, double n2, double n3){
+        this.name = nome;
+        this.registration = matricula;
+        this.grade1 = n1;
+        this.grade2 = n2;
+        this.grade3 = n3;
+    }
+
     public String getName(){
         return this.name;
     }
@@ -12,5 +20,9 @@ public class Aluno {
 
     public double AveragingCalculation(){
         return (this.grade1 + this.grade2 + this.grade3) / 3;
+    }
+
+    public String toString(){
+        return "Nome: " + this.name + "\nMatrícula: " + this.registration + "\nMédia: " + this.AveragingCalculation();
     }
 }
